ecd20b07ccee8730ed679fd0b543ebb889fcabbf
Sesana Federico <sesana.federico@osdbsesto>
2021-11-11 11:42:45 +0100

    b kj

--- es3.js
+++ es3.js
@@ -1,3 +1,5 @@
+var vals = [];
+
 $("button").click(function() {
     var input1 = $('#num1');
     var input2 = $('#num2');
@@ -25,6 +27,10 @@ $("button").click(function() {
         };
         var string = "<tr><td>" + n1 + "</td><td>" + sign + "</td><td>" + n2 + "</td><td>" + result + "</td></tr>";
         $('#tab').append(string);
+
+        vals.push(input1.val() + "; " + sign + "; " + input2.val() + "; " + result);
+        storage.setItem("1", vals);
+        console.log(storage.getItem("1"));
         input1.val("");
         input2.val("");
     }
